8f2afe354143a7d62d1eda3906a9ea54e8d7bfe9
mmalnoaimi <m.alnoaimi08@gmail.com>
2025-09-02 17:59:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ import uuid
 
 #Zahra
 
-#Mariam
-
+#Maryam
+def recipes_by_ingredients():
+    ingredients = input('Which ingredient would you like to search for?  ').lower()
+    found = False 
+    with open(FILENAME, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if ingredients in row['Ingredients'].lower():
+                print(f"\n {row['Name']}")
+                print(f"Ingredients: {row['Ingredients']}")
+                print(f"Instructions: {row['Instructions']}\n")
+                print(f"Preperation Time: {row['PrepTime']}")
+                found = True 
+            if not found:
+                print(f" No recipes found with '{ingredients}' ")
 #Sayed Hussain
 
 #Waseem
