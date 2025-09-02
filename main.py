dca665b5933f00274911ab906df8ae1c342977f3
Hussain Darwish <63611452+FearLess93@users.noreply.github.com>
2025-09-02 20:53:23 +0300

    Improve error handling in view_random_recipe

--- main.py
+++ main.py
@@ -156,16 +156,16 @@ def add_recipe():
 
 def view_random_recipe():
     """"Generate A Random Recipe from Available Recipes"""
-    try:
-        index = rd.randint(0, len(recipes) - 1)
-        i = 1
-        while i == 1:  
+    i = 1
+    while i == 1:
+        try:
+            index = rd.randint(0, len(recipes) - 1)
             print(f"\n=========== Here is a Random Recipe you can try ===============\n\n")
             print(recipes[index])
-            i = (int(input("Enter '1' to generate another random recipe or '0' to exit ")))
-    
-    except:
-        print("Please Retry again")
+            i = (int(input("Enter '1' to generate another random recipe or '0' to exit: ")))
+        except ValueError:
+            if i > 1 or (i != 1 and i != 0):
+                i = int(input("Please Enter Correct Values (1 or 0): "))
 
 def rate_recipe():
     recipes = ()
