1eac0ab328090b72d5167dae90ce64ab272b5dc1
zm422252 <zm422252@gmail.com>
2025-09-02 18:41:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import uuid
 
 # Hussain Darwish
 
-#Zahra
+#sayed hussain
 
 #Mariam
 
-#Sayed Hussain
+#zahraa
+recipes = [{'name':'cookies','prep_time':'25'},{'name':'cake','prep_time':'55'}]
 
+def view_all_recipes(i):
+    '''View all recipies in the database.'''
+    if len(recipes) > 0:
+        for i in recipes:
+            print(f"Recipe Name: {i['name']} \nPreperation Time: {i['prep_time']} minutes \n\n")
+    else:
+        print('No Recipes Found')
+
+view_all_recipes(recipes)
 #Waseem
