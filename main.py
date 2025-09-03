2275810dbee4604da15331d356670a35d5c0bfa5
Hussain Darwish <63611452+FearLess93@users.noreply.github.com>
2025-09-03 18:18:10 +0300

    Add trailing spaces to input prompts
    Updated input prompts to include trailing spaces for better readability.

--- main.py
+++ main.py
@@ -92,14 +92,14 @@ def add_recipe():
 
     print('\nWell, it is time to add your lovely recipe now!') #tell the user that they have loaded their file with a nice welcoming message
     recipe_id = str(uuid.uuid4()) # we here are creating a unique id for each recipe. this can help us in the future.
-    name = input('What is the name of your recipe 👀?')
+    name = input('What is the name of your recipe 👀? ')
 
-    ingredients = input('\nWhat ingredients would we need for this dish? 🥕 \n(separate them by commas)')
+    ingredients = input('\nWhat ingredients would we need for this dish? 🥕 \n(separate them by commas) ')
     valid_prep = True # creating the loop turn on condition
     
     while valid_prep:
         try:
-            prep_time = int(input('\nand how many mins would this dish take to get ready ⏲️? \n numbers only please...'))
+            prep_time = int(input('\nand how many mins would this dish take to get ready ⏲️? \n numbers only please... '))
             if type(prep_time) is int:
                 valid_prep = False # ending the loop
         except ValueError:
@@ -114,7 +114,7 @@ def add_recipe():
         inst = input('\n Please enter the step here: ')
         cooking_instructions.append(inst)
         print('Step Added succesfuly')
-        inst_done = input('\nPress "Enter" to add another step, write "Done" to confirm your instructions')
+        inst_done = input('\nPress "Enter" to add another step, write "Done" to confirm your instructions ')
         if inst_done == 'Done':
             no_instructions = False #end loop when "Done" is inputted
 
@@ -122,7 +122,7 @@ def add_recipe():
     #No we will create a valid diffuclty case
     valid_difficulty = ['easy','medium','hard']
     while True:
-        difficulty = input('\nWhat is the difficulty of this wonderful dish🫢 [Easy, Medium, Hard]\nChoose one of the options above!')
+        difficulty = input('\nWhat is the difficulty of this wonderful dish🫢 [Easy, Medium, Hard]\nChoose one of the options above! ')
         if difficulty.lower() in valid_difficulty:
             break
         else:
@@ -131,7 +131,7 @@ def add_recipe():
         #Now will create a category
     valid_category = ['breakfast','lunch','dinner', 'dessert']
     while True:
-        category = input('\nWhat do you consider this meal to be?😋 [Breakfast, Lunch, Dinner, Dessert]\nChoose one of the options above!')
+        category = input('\nWhat do you consider this meal to be?😋 [Breakfast, Lunch, Dinner, Dessert]\nChoose one of the options above! ')
         if category.lower() in valid_category:
             break
         else:
