474e53e18acdf0228d32737ad79cf9f1596566f7
Hussain Darwish <63611452+FearLess93@users.noreply.github.com>
2025-09-07 18:14:56 +0300

    Improve user prompts and error messages

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ def view_all_recipes():
             print(f"   Instructions: {recipe['cooking_instructions']}")
             rating = recipe['rating'] if recipe['rating'] else 'Not rated'
             print(f"   Rating: {rating}")
+            last_cooked = recipe['last_cooked'] if recipe['last_cooked'] else 'Not cooked yet'
+            print(f"   Last Cooked: {last_cooked}")
             print("-" * 50)
             
     except FileNotFoundError:
@@ -116,14 +118,14 @@ def add_recipe():
 
     print('\nWell, it is time to add your lovely recipe now!') #tell the user that they have loaded their file with a nice welcoming message
     recipe_id = str(uuid.uuid4()) # we here are creating a unique id for each recipe. this can help us in the future.
-    name = input('What is the name of your recipe 👀?')
+    name = input('What is the name of your recipe 👀? ')
 
-    ingredients = input('\nWhat ingredients would we need for this dish? 🥕 \n(separate them by commas)')
+    ingredients = input('\nWhat ingredients would we need for this dish? 🥕 \n(separate them by commas. Eg: milk 40ml, coffee 30g, .. etc) ')
     valid_prep = True # creating the loop turn on condition
     
     while valid_prep:
         try:
-            prep_time = int(input('\nand how many mins would this dish take to get ready ⏲️? \n numbers only please...'))
+            prep_time = int(input('\nand how many mins would this dish take to get ready ⏲️? \n numbers only please: '))
             if type(prep_time) is int:
                 valid_prep = False # ending the loop
         except ValueError:
@@ -138,15 +140,15 @@ def add_recipe():
         inst = input('\n Please enter the step here: ')
         cooking_instructions.append(inst)
         print('Step Added succesfuly')
-        inst_done = input('\nPress "Enter" to add another step, write "Done" to confirm your instructions')
-        if inst_done == 'Done':
-            no_instructions = False #end loop when "Done" is inputted
+        inst_done = input('\nPress "Enter" to add another step, write "Done" to confirm your instructions: ')
+        if inst_done.lower() == 'done':
+            no_instructions = False #end loop when "done" is inputted
 
     
-    #No we will create a valid diffuclty case
+    #Now we will create a valid diffuclty case
     valid_difficulty = ['easy','medium','hard']
     while True:
-        difficulty = input('\nWhat is the difficulty of this wonderful dish🫢 [Easy, Medium, Hard]\nChoose one of the options above!')
+        difficulty = input('\nWhat is the difficulty of this wonderful dish🫢 [Easy, Medium, Hard]\nChoose one of the options above!: ')
         if difficulty.lower() in valid_difficulty:
             break
         else:
@@ -155,7 +157,7 @@ def add_recipe():
         #Now will create a category
     valid_category = ['breakfast','lunch','dinner', 'dessert']
     while True:
-        category = input('\nWhat do you consider this meal to be?😋 [Breakfast, Lunch, Dinner, Dessert]\nChoose one of the options above!')
+        category = input('\nWhat do you consider this meal to be?😋 [Breakfast, Lunch, Dinner, Dessert]\nChoose one of the options above!: ')
         if category.lower() in valid_category:
             break
         else:
